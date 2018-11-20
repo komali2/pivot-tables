a2965a8e5c46d888430689cd86e82ccefe8790dc
Caleb Rogers <rogersjcaleb@gmail.com>
2018-11-19 17:19:50 -0800

    Pivot table appears to work

---
+++ src/Pivot.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PivotTableUI from 'react-pivottable/PivotTableUI';
+import 'react-pivottable/pivottable.css';
+import TableRenderers from 'react-pivottable/TableRenderers';
+import Plot from 'react-plotly.js';
+import createPlotlyRenderers from 'react-pivottable/PlotlyRenderers';
+
+// create Plotly renderers via dependency injection
+const PlotlyRenderers = createPlotlyRenderers(Plot);
+
+// see documentation for supported input formats
+const data = [['attribute', 'attribute2'], ['value1', 'value2']];
+
+class Pivot extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = props;
+    }
+
+    render() {
+        return (
+            <PivotTableUI
+                data={data}
+                onChange={s => this.setState(s)}
+                renderers={Object.assign({}, TableRenderers, PlotlyRenderers)}
+                {...this.state}
+            />
+        );
+    }
+}
+export default Pivot;
